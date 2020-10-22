d5b635abc48db4a472e5d79aee6af13f20100f5d
anchobi-no <shiozaki.ryosuke@jp.panasonic.com>
2020-10-22 17:23:41 +0900

    catkin_ws push

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shiozaki/git/ai_race/catkin_ws/src
+# Build directory: /home/shiozaki/git/ai_race/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("tutorial1")
