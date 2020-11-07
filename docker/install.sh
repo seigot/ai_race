33d4ce7c05c9de278a81c98bb6d6cdaa57adcdb7
Your Name <you@example.com>
2020-11-08 02:37:50 +0900

    update Dockerfile install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -2,29 +2,6 @@
 
 HOME=/home/jetson
 
-#function install_ros(){
-#    cd $HOME
-#    git clone https://github.com/karaage0703/jetson-nano-tools
-#    cd jetson-nano-tools
-#    ./install-ros-melodic.sh
-#}
-
-#function install_package(){
-#    # joint state controller
-#    sudo apt install -y ros-melodic-ros-control ros-melodic-ros-controllers  ros-melodic-joint-state-controller ros-melodic-effort-controllers ros-melodic-position-controllers ros-melodic-joint-trajectory-controller
-#    # gazebo
-#    sudo apt-get install -y gazebo9
-#    sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-#    wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-#    sudo apt-get update -y
-#    sudo apt-get install -y ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
-#    echo "export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ai_race/catkin_ws/src:~/ai_race/catkin_ws/src/sim_world/models" >> ~/.bashrc
-#    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/ai_race/catkin_ws/src:~/ai_race/catkin_ws/src/sim_world/models
-#    # camera image
-#    sudo apt-get install -y ros-melodic-uvc-camera
-#    sudo apt-get install -y ros-melodic-image-*
-#}
-
 function setup_package(){
     echo "source /opt/ros/melodic/setup.bash" >> $HOME/.bashrc
     source /opt/ros/melodic/setup.bash
@@ -35,10 +12,10 @@ function setup_package(){
     source devel/setup.sh
 }
 
-#install_ros
-#install_package
-setup_package
+function setup_ai_race_env(){
+    mkdir -p $HOME/Images_from_rosbag
+}
 
-#コンテナを起動し続ける
-#tail -f /dev/null
+setup_package
+setup_ai_race_env
 
