aff2a1cbb92238b837ac82fc88d05ea76f6e9402
Your Name <you@example.com>
2020-11-07 22:20:41 +0900

    update Dockerfile for MachineLearning library

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# torch2trt setup
+python /tmp/torch2trt/setup.py install
+python3 /tmp/torch2trt/setup.py install
+
+# tmp
+pip3 install future
+pip3 install torchvision==0.2.2
+
+python3 -c 'import torch; print(torch.__version__) '
+python3 -c "import torchvision;print(torchvision.__version__);"
+python3 -c "import sklearn;print(sklearn.__version__);"
+python3 -c "import pandas as pd ;print(pd.__version__);"
+python3 -c "import cv2 ;print(cv2.__version__);"
+
+python -c 'import torch; print(torch.__version__) '
+python -c "import torchvision;print(torchvision.__version__);"
+python -c "import cv2 ;print(cv2.__version__);"
 
 echo "start process.."
 
