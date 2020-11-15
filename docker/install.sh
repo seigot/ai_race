3d848a4b792e016da2bef2b35f316a0592f09666
seigot <s.takada.3o3@gmail.com>
2020-11-16 01:57:32 +0900

    update Dockerfile for judge server

--- docker/install.sh
+++ docker/install.sh
@@ -15,14 +15,12 @@ function setup_package(){
 
 function setup_ai_race_env(){
 
+    # general praparation
     mkdir -p $HOME_JETSON/Images_from_rosbag
 
     # setup ai_race_data_sample
     cd $HOME_JETSON
     git clone http://github.com/seigot/ai_race_data_sample
-
-    # delete unnecessary file
-    #rm -fr $HOME_JETSON/catkin_ws # delete unnecessary directory..
 }
 
 setup_package
