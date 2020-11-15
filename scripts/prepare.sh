7d49ce16a0160b17d7be22dd63fbd25b1a84d1fc
seigot <s.takada.3o3@gmail.com>
2020-11-15 20:29:11 +0900

    update game prepare script to launch judge server

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,17 @@
 #!/bin/bash -x
 
+# エラー時はエラー終了する
+set -e
+# 未定義変数参照時はエラーにする
+set -u
+
 echo "start prepare.sh"
 
-roslaunch user_tutorial2 wheel_robot.launch
+# init judge server, timer window, etc
+gnome-terminal -e "python3 judge/judgeServer.py"
+sleep 1
+gnome-terminal -e "python3 judge/timer.py"
+# [future work] if necessary, register some data to server here.
+
+# init simulator, course and vehicle
+roslaunch user_tutorial1 wheel_robot.launch
