ae25a0832b97ee3995b4ad872dca3ed65120efd3
Daisuke-Kubo <da1i13su16ke@gmail.com>
2020-11-17 23:01:50 +0900

    Linuxマシン用インストールスクリプト（検証中）

---
+++ scripts/setup/auto_setup_for_Linux.sh
@@ -0,0 +1,186 @@
+#!/bin/bash -x
+
+# エラーが起こったら異常終了させる
+set -E
+
+function failure(){
+    echo "error end!!"
+    exit 1
+}
+
+# エラー発生時にコールする関数を設定 
+trap failure ERR
+
+#DEBIAN_FRONTEND=noninteractive
+
+function setup_swap_file(){
+    cd ~
+    if [ -d "./installSwapfile" ]; then
+	echo "skip"
+	return 0
+    fi
+    git clone https://github.com/JetsonHacksNano/installSwapfile
+    cd installSwapfile
+    ./installSwapfile.sh
+    # SWAP領域が増えていることを確認
+    free -mh
+}
+
+function install_basic_package(){
+    sudo apt-get update
+    sudo apt-get install -y net-tools git
+    sudo apt-get install -y python-pip
+    # install pyqt5 and NumPy
+    sudo apt-get install -y python3-pip
+    sudo apt-get install -y python3-pyqt5
+    pip3 install --upgrade pip
+    pip3 install numpy
+    # for judge server
+    pip3 install flask
+}
+
+function install_ros(){
+    cd ~
+    # sudo rm -rf jetson-nano-tools
+    # git clone https://github.com/karaage0703/jetson-nano-tools
+    # cd jetson-nano-tools
+    # ./install-ros-melodic.sh
+    sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+    sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+    sudo apt update
+    sudo apt install ros-melodic-desktop-full
+    sudo apt-get install -y python-catkin-tools
+    mkdir -p ~/catkin_ws/src
+    cd ~/catkin_ws
+    source /opt/ros/melodic/setup.bash
+    catkin init
+    catkin build
+    sh -c "echo \"source ~/catkin_ws/devel/setup.bash\" >> ~/.bashrc"
+    source ~/.bashrc
+    sudo apt-get install -y python-rosdep python-rosinstall python-rosinstall-generator build-essential
+    sudo rosdep init
+    rosdep update
+    echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+    source /opt/ros/melodic/setup.bash
+}
+
+function install_ros_related_packages(){
+    # joint state controller, and ros package
+    sudo apt install -y ros-melodic-ros-control ros-melodic-ros-controllers  ros-melodic-joint-state-controller ros-melodic-effort-controllers ros-melodic-position-controllers ros-melodic-joint-trajectory-controller
+    # gazebo
+    sudo apt-get install -y gazebo9
+    sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+    wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+    sudo apt-get update -y
+    sudo apt-get install -y ros-melodic-gazebo-ros-pkgs ros-melodic-gazebo-ros-control
+    echo "export GAZEBO_MODEL_PATH=:/home/jetson/catkin_ws/src/ai_race/ai_race:/home/jetson/catkin_ws/src/ai_race/ai_race/sim_world/models" >> ~/.bashrc
+    export GAZEBO_MODEL_PATH=:/home/jetson/catkin_ws/src/ai_race/ai_race:/home/jetson/catkin_ws/src/ai_race/ai_race/sim_world/models
+    # camera image
+    sudo apt-get install -y ros-melodic-uvc-camera
+    sudo apt-get install -y ros-melodic-image-*
+}
+
+function install_torch(){
+    ### pytorch from pip image (v1.4)
+    wget https://nvidia.box.com/shared/static/yhlmaie35hu8jv2xzvtxsh0rrpcu97yj.whl -O torch-1.4.0-cp27-cp27mu-linux_aarch64.whl
+    sudo apt-get install -y python-pip libopenblas-base libopenmpi-dev
+    python -m pip install torch-1.4.0-cp27-cp27mu-linux_aarch64.whl
+    wget https://nvidia.box.com/shared/static/c3d7vm4gcs9m728j6o5vjay2jdedqb55.whl -O torch-1.4.0-cp36-cp36m-linux_aarch64.whl
+    sudo apt-get install -y python3-pip libopenblas-base libopenmpi-dev
+    pip3 install torch-1.4.0-cp36-cp36m-linux_aarch64.whl
+}
+
+function install_torchvision(){
+    ### torch vision (v0.2.2)
+    # https://forums.developer.nvidia.com/t/pytorch-for-jetson-version-1-7-0-now-available/72048
+    pip install future
+    pip3 install future
+    sudo apt-get install libjpeg-dev zlib1g-dev libpython3-dev libavcodec-dev libavformat-dev libswscale-dev
+
+    cd ~
+    sudo rm -rf torchvision
+    git clone --branch v0.5.0 https://github.com/pytorch/vision torchvision
+    cd torchvision
+    export BUILD_VERSION=0.2.2
+    sudo python setup.py install
+    sudo python3 setup.py install
+    cd ../
+    pip install 'pillow<7'
+    
+    #pip install future
+    #pip install torchvision==0.2.2
+    #pip3 install future
+    #pip3 install torchvision==0.2.2
+}
+
+function install_torch2trt(){
+    ### torch2trt
+    cd ~
+    sudo rm -rf torch2trt
+    git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+    cd torch2trt
+    sudo python setup.py install
+    sudo python3 setup.py install
+}
+
+function install_sklearn(){
+    ### sklearn python3
+    pip3 install scikit-learn
+    # pip3 install matplotlib
+    #sudo apt-get -y install python3-tk
+}
+
+function install_numpy(){
+    echo "skip"
+    ### pandas python2,3 (defaultを使えばよい)
+    #pip3 install cython
+    #pip3 install numpy
+    pip3 install -U pandas
+}
+
+function install_opencv(){
+    pip3 install opencv-python==3.4.10
+    ### opencv python
+    ### opencv python はソースからビルドする必要がある. 8～10時間ほど掛かる.
+    # cd ~
+    # sudo rm -rf nano_build_opencv
+    # git clone https://github.com/mdegans/nano_build_opencv
+    # cd nano_build_opencv
+    # yes | ./build_opencv.sh 3.4.10
+}
+
+function setup_this_repository(){
+    cd ~/catkin_ws/src
+    sudo rm -rf ai_race
+    git clone http://github.com/seigot/ai_race
+    cd ~/catkin_ws
+    catkin build
+    source devel/setup.bash
+    echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+}
+
+function check_lib_version(){
+    python3 -c 'import torch; print(torch.__version__) '
+    python3 -c "import torchvision;print(torchvision.__version__);"
+    python3 -c "import cv2 ;print(cv2.__version__);"
+    python3 -c "import sklearn;print(sklearn.__version__);"
+    python3 -c "import pandas as pd ;print(pd.__version__);"
+    python -c 'import torch; print(torch.__version__) '
+    python -c "import torchvision;print(torchvision.__version__);"
+    python -c "import cv2 ;print(cv2.__version__);"
+}
+
+echo "start install"
+# setup_swap_file
+install_basic_package
+install_ros
+install_ros_related_packages
+install_torch
+install_torchvision
+# install_torch2trt
+install_sklearn
+install_numpy
+install_opencv
+setup_this_repository
+check_lib_version
+echo "finish install"
