8d3bdc3706f2890e977a5a31e93e4c3b8a05b821
seigot <s.takada.3o3@gmail.com>
2020-11-20 18:00:08 +0900

    add request to judge script
     option
       -h host address,
          use like.. " -h 192.168.2.101:5000 "
          default: localhost:5000
    
       -s change state parameter,
          use like.. "-s start"
          parameter
             start : start timer
             stop  : stop timer
             init  : init timer

---
+++ judge/request_to_judge.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+##### request to judge script #####
+# option
+#   -h host address,
+#      use like.. " -h 192.168.2.101:5000 " 
+#      default: localhost:5000
+#
+#   -s change state parameter,
+#      use like.. "-s start"
+#      parameter  
+#         start : start timer
+#         stop  : stop timer
+#         init  : init timer
+#
+###################################
+
+# default parameter
+HOSTADDR="localhost:5000"
+REQ_STATE="start"
+
+while getopts h:s: OPT
+do
+    case $OPT in
+        "h" ) HOSTADDR=$OPTARG; echo "-h host = $HOSTADDR !!";;
+        "s" ) REQ_STATE=$OPTARG; echo "-s state = $REQ_STATE !!";;
+    esac
+done
+
+JUDGESERVER_REQUEST_URL="http://${HOSTADDR}/judgeserver/request"
+
+########### script ########################
+# set state to "running"
+echo "=================set state "running"======================"
+
+if [ "$REQ_STATE" == "start" ]; then
+    curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"change_state":"start"}' ${JUDGESERVER_REQUEST_URL}
+elif [ "$REQ_STATE" == "stop" ]; then
+    curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"change_state":"stop"}' ${JUDGESERVER_REQUEST_URL}
+elif [ "$REQ_STATE" == "init" ]; then
+    curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"change_state":"init"}' ${JUDGESERVER_REQUEST_URL}
+else
+    echo "invalid parameter... ${HOSTADDR}, ${REQ_STATE}"
+fi
+
+
+
