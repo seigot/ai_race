88ece62033ccf5f9f118fb00ab77c9ed8e4cd860
seigot <s.takada.3o3@gmail.com>
2020-11-21 00:35:05 +0900

    add stop.sh, to kill process.

---
+++ scripts/stop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function try_kill_process(){
+    PROCESS_NAME=$1
+    if [ -z "$PROCESS_NAME" ]; then
+	return 0
+    fi
+    
+    PROCESS_ID=`ps -e -o pid,cmd | grep ${PROCESS_NAME} | grep -v grep | awk '{print $1}'`
+    if [ -z "$PROCESS_ID" ]; then
+	echo "no process like... ${PROCESS_NAME}"
+	return 0
+    fi
+    kill $PROCESS_ID
+}
+
+function stop_process(){    
+    try_kill_process "roscore"
+    try_kill_process "judgeServer.py"
+    try_kill_process "timer.py"
+    try_kill_process "window_management"
+    try_kill_process "keyboard_con_pygame"
+    try_kill_process "wheel_robot"
+}
+
+stop_process
