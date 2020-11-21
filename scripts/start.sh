ce9b32d41fd3c12d78ca29b8e6646648e684998a
seigot <s.takada.3o3@gmail.com>
2020-11-21 23:59:32 +0900

    update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,6 @@
 
 echo "start start.sh"
 
-python3 judge/timer.py &
-
 pushd /home/jetson/ai_race/catkin_ws/src/user_tutorial2/scripts
 python inference_from_image.py --pretrained_model /home/jetson/ai_race/catkin_ws/src/experiments/models/checkpoints/sim_race_joycon_ResNet18_6_epoch=20.pth
 popd
