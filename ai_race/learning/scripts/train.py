501f2cba79bbf97d3862b29aff42fe5876e9b924
kyad <53517611+kyad@users.noreply.github.com>
2020-12-31 06:33:11 +0000

    Remove unused code

--- ai_race/learning/scripts/train.py
+++ ai_race/learning/scripts/train.py
@@ -2,7 +2,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
 import torchvision
 import torchvision.transforms as transforms
@@ -16,7 +15,6 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
-from sklearn.model_selection import train_test_split
 from MyDataSet import MyDataset
 from samplenet import SampleNet, SimpleNet
 
