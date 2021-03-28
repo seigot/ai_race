5974180521aa83759d09112a8599cef37e2e5a98
k-onishi <kazuki.onishi1108@gmail.com>
2021-03-28 16:18:34 +0900

    add cool time and feature of getting collided object

--- ai_race/sim_environment/scripts/collision_surveillance.py
+++ ai_race/sim_environment/scripts/collision_surveillance.py
@@ -18,6 +18,10 @@ CAR_LENGTH = 0.4
 CAR_WIDTH = 0.2
 CONE_WIDTH = 0.2
 
+# 一度検出した後に次に検出可能になるまでの時間
+# mainに書いているけどクラスに書いた方が自然な気がする
+COOL_TIME = 30
+
 #TODO: 引数名を真面目に考える
 #降順にして返す、sortでやったほうがよい気がしてきた
 def get_sorted_pair(a, b):
@@ -35,13 +39,16 @@ def is_in_range(value, minimum, maximum):
 
 class CollisionDetector(object):
 
-    def __init__(self):
+    def __init__(self, cool_time=0):
+        self.cool_time = cool_time
+        self.cool_time_left = 0
         rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback, queue_size=1)
         self.data = None
-        self.obeject_positions = []
+        self.obeject_positions = {}
 
     def callback(self, data):
         self.data = data
+        self.cool_time_left -= 1
 
     def is_collided_rect_and_rect(
             self,
@@ -76,40 +83,45 @@ class CollisionDetector(object):
         y = data.pose[pos].position.y
         # 障害物の座標が分かっていないなら取得する
         if len(self.obeject_positions) != len(CONES):
-            self.obeject_positions = []
+            self.obeject_positions = {}
             for object_name in CONES:
                 pos = data.name.index(object_name)
-                self.obeject_positions.append([
+                self.obeject_positions[object_name] = [
                     data.pose[pos].position.x,
                     data.pose[pos].position.y,
-                ])
+                ]
         return x, y
 
     # 障害物判定
     @property
-    def is_collided(self):
-        if self.data is None:
-            return False
+    def collided_object(self):
+        if self.data is None or self.cool_time_left > 0:
+            return None
         car_x, car_y = self.get_position(self.data)
-        for object_x, object_y in self.obeject_positions:
+        for object_name, object_position in self.obeject_positions.items():
+            object_x, object_y = object_position
             if self.is_collided_rect_and_rect(
                     car_x + CAR_WIDTH / 2, car_x - CAR_WIDTH / 2,
                     car_y + CAR_LENGTH / 2, car_y - CAR_LENGTH / 2,
                     object_x + CONE_WIDTH / 2, object_x - CONE_WIDTH / 2,
                     object_y + CONE_WIDTH / 2, object_y - CONE_WIDTH / 2
                     ):
-                return True
-        return False
+                self.cool_time_left = self.cool_time
+                return object_name
+        return None
 
 if __name__ == '__main__':
     try:
+        cool_time = 0
         rospy.init_node('collision_surveillance', anonymous=True)
-        collision_detector = CollisionDetector()
+        collision_detector = CollisionDetector(COOL_TIME)
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
-            if collision_detector.is_collided:
-                print("collided")
+            collided_object = collision_detector.collided_object
+            if collided_object is not None:
+                print("collided: {}".format(collided_object))
             else:
                 print("not collided")
+                pass
     except rospy.ROSInterruptException:
         pass
